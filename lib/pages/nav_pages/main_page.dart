aad9fe096eb0b27205a3cf93c657a40e67940b25
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-16 19:00:09 +0900

    bottom_nav_ui

--- lib/pages/nav_pages/main_page.dart
+++ lib/pages/nav_pages/main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app/pages/nav_pages/bar_item_page.dart';
+import 'package:travel_app/pages/nav_pages/home_page.dart';
 import 'package:travel_app/pages/nav_pages/my_page.dart';
 import 'package:travel_app/pages/nav_pages/search_page.dart';
 
@@ -12,14 +13,36 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   List pages = [
+    const HomePage(),
     const BarItemPage(),
     const SearchPage(),
     const MyPage(),
   ];
+
+  int currentIndex = 0;
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.apps), label: 'Home'),
           BottomNavigationBarItem(

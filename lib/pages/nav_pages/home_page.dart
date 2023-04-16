003726e8d49bc324e331060b9ef07ae73d555862
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-16 19:25:03 +0900

    TabBar with TickerProvider

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,9 +10,59 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+// with TickerProviderStateMixin - uses tabbar
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const Icon(
+          Icons.menu,
+          size: 30,
+          color: Colors.black,
+        ),
+        actions: [],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 20),
+            child: AppLargeText(
+              text: "Discover",
+              color: Colors.black,
+            ),
+          ),
+
+          const SizedBox(height: 30),
+          // tab_bar
+          Container(
+            child: TabBar(
+              controller: _tabController,
+              tabs: const [
+                Tab(text: "Places"),
+                Tab(text: "Inspiration"),
+                Tab(text: "Emotions"),
+              ],
+            ),
+          ),
+          Container(
+            width: double.maxFinite,
+            height: 300,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                Text("Hi"),
+                Text("There"),
+                Text("Bye"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

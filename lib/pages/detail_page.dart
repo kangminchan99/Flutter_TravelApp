7d002bc8a3a07581bd3102db5c7ac2f757372ff5
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-17 19:30:17 +0900

    rating star ui

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/app_colors.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,6 +11,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +25,7 @@ class _DetailPageState extends State<DetailPage> {
               right: 0,
               child: Container(
                 width: double.maxFinite,
-                height: 300,
+                height: 350,
                 decoration: const BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage("images/mexico_beach.jpeg"),
@@ -42,6 +46,71 @@ class _DetailPageState extends State<DetailPage> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                          text: 'Hidden Beach',
+                          color: Colors.black,
+                        ),
+                        AppLargeText(
+                          text: '\$ 2160',
+                          color: Colors.blueAccent,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        const Icon(Icons.location_on, color: Colors.blue),
+                        const SizedBox(width: 5),
+                        AppText(
+                          text: "Mexico, Marietas Islands",
+                          color: Colors.blueAccent.withOpacity(0.7),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    // star rating
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: index < gottenStars
+                                  ? Colors.yellow
+                                  : Colors.grey,
+                            );
+                          }),
+                        ),
+                        const SizedBox(width: 10),
+                        AppText(
+                          text: '(4.0)',
+                          color: Colors.grey,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],

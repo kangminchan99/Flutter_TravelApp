0b3bcd794119c6f14e5ff776ef61d8c658c2fd59
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-21 22:07:15 +0900

    linked app & cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubits.dart';
 import 'package:travel_app/pages/detail_page.dart';
 import 'package:travel_app/pages/nav_pages/main_page.dart';
 import 'package:travel_app/pages/welcome_page.dart';
@@ -18,7 +20,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DetailPage(),
+      home: BlocProvider<AppCubits>(
+        create: (context) => AppCubits(),
+      ),
     );
   }
 }

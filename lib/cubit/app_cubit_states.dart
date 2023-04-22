e32759413c120d9e2d2573d0225d1f9e6c6ee8d8
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-22 21:45:25 +0900

    .

--- lib/cubit/app_cubit_states.dart
+++ lib/cubit/app_cubit_states.dart
@@ -6,11 +6,21 @@ abstract class CubitStates extends Equatable {}
 class InitialState extends CubitStates {
   @override
   // 변수를 기반으로 새 빌드를 다시 트리거하고 싶으면 변수를 설정하고, []안에 값을 넣어준다.
-  List<Object?> get props => [];
+  List<Object> get props => [];
 }
 
 // 초기 상태가 완료되면 새 상태(welcomepage)를 트리거한다.
 class WelcomeState extends CubitStates {
   @override
-  List<Object?> get props => [];
+  List<Object> get props => [];
+}
+
+class LoadingState extends CubitStates {
+  @override
+  List<Object> get props => [];
+}
+
+class LoadedState extends CubitStates {
+  @override
+  List<Object> get props => [];
 }

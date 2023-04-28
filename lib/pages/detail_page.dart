0e41bfe8d1fd9bab22cddd209e31b589724407c1
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-04-29 02:55:03 +0900

    data dynamic on detail_page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travel_app/cubit/app_cubit_states.dart';
 import 'package:travel_app/cubit/app_cubits.dart';
 import 'package:travel_app/misc/app_colors.dart';
 import 'package:travel_app/widgets/app_buttons.dart';
@@ -19,183 +20,189 @@ class _DetailPageState extends State<DetailPage> {
   int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.maxFinite,
-        height: double.maxFinite,
-        child: Stack(
-          children: [
-            Positioned(
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.maxFinite,
-                height: 350,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/mexico_beach.jpeg"),
-                      fit: BoxFit.cover),
+    return BlocBuilder<AppCubits, CubitStates>(
+      builder: (context, state) {
+        DetailState detail = state as DetailState;
+        return Scaffold(
+          body: Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    width: double.maxFinite,
+                    height: 350,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "http://mark.bslmeiyu.com/uploads/${detail.place.img}"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: 50,
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // go back
-                      BlocProvider.of<AppCubits>(context).goHome();
-                    },
-                    icon: const Icon(
-                      Icons.menu,
+                Positioned(
+                  left: 20,
+                  top: 50,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          // go back
+                          BlocProvider.of<AppCubits>(context).goHome();
+                        },
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 260,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.only(left: 20, right: 20, top: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 500,
+                    decoration: const BoxDecoration(
                       color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 260,
-              child: Container(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
-                width: MediaQuery.of(context).size.width,
-                height: 500,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        AppLargeText(
-                          text: 'Hidden Beach',
-                          color: Colors.black,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AppLargeText(
+                              text: detail.place.img,
+                              color: Colors.black,
+                            ),
+                            AppLargeText(
+                              text: '\$${detail.place.price.toString()}',
+                              color: Colors.blueAccent,
+                            ),
+                          ],
                         ),
-                        AppLargeText(
-                          text: '\$ 2160',
-                          color: Colors.blueAccent,
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            const Icon(Icons.location_on, color: Colors.blue),
+                            const SizedBox(width: 5),
+                            AppText(
+                              text: detail.place.location,
+                              color: Colors.blueAccent.withOpacity(0.7),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        const Icon(Icons.location_on, color: Colors.blue),
-                        const SizedBox(width: 5),
+                        const SizedBox(height: 20),
+                        // star rating
+                        Row(
+                          children: [
+                            Wrap(
+                              children: List.generate(5, (index) {
+                                return Icon(
+                                  Icons.star,
+                                  color: index < detail.place.stars
+                                      ? Colors.yellow
+                                      : Colors.grey,
+                                );
+                              }),
+                            ),
+                            const SizedBox(width: 10),
+                            AppText(
+                              text: '(5.0)',
+                              color: Colors.grey,
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 25),
+                        AppLargeText(
+                          text: 'People',
+                          color: Colors.black.withOpacity(0.8),
+                          size: 20,
+                        ),
+                        const SizedBox(height: 5),
                         AppText(
-                          text: "Mexico, Marietas Islands",
-                          color: Colors.blueAccent.withOpacity(0.7),
+                          text: 'Number of prople in your group',
+                          color: Colors.grey,
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    // star rating
-                    Row(
-                      children: [
+                        const SizedBox(height: 10),
                         Wrap(
                           children: List.generate(5, (index) {
-                            return Icon(
-                              Icons.star,
-                              color: index < gottenStars
-                                  ? Colors.yellow
-                                  : Colors.grey,
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  selectedIndex = index;
+                                });
+                              },
+                              child: Container(
+                                margin: const EdgeInsets.only(right: 10),
+                                child: AppButtons(
+                                  color: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.black,
+                                  backgroundColor: selectedIndex == index
+                                      ? Colors.black
+                                      : AppColors.buttonBackground,
+                                  boderColor: selectedIndex == index
+                                      ? Colors.black
+                                      : AppColors.buttonBackground,
+                                  size: 50,
+                                  text: (index + 1).toString(),
+                                ),
+                              ),
                             );
                           }),
                         ),
-                        const SizedBox(width: 10),
+                        const SizedBox(height: 20),
+                        AppLargeText(
+                          text: 'Description',
+                          color: Colors.black.withOpacity(0.8),
+                          size: 20,
+                        ),
+                        const SizedBox(height: 5),
                         AppText(
-                          text: '(4.0)',
+                          text: detail.place.description,
                           color: Colors.grey,
                         )
                       ],
                     ),
-                    const SizedBox(height: 25),
-                    AppLargeText(
-                      text: 'People',
-                      color: Colors.black.withOpacity(0.8),
-                      size: 20,
-                    ),
-                    const SizedBox(height: 5),
-                    AppText(
-                      text: 'Number of prople in your group',
-                      color: Colors.grey,
-                    ),
-                    const SizedBox(height: 10),
-                    Wrap(
-                      children: List.generate(5, (index) {
-                        return InkWell(
-                          onTap: () {
-                            setState(() {
-                              selectedIndex = index;
-                            });
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.only(right: 10),
-                            child: AppButtons(
-                              color: selectedIndex == index
-                                  ? Colors.white
-                                  : Colors.black,
-                              backgroundColor: selectedIndex == index
-                                  ? Colors.black
-                                  : AppColors.buttonBackground,
-                              boderColor: selectedIndex == index
-                                  ? Colors.black
-                                  : AppColors.buttonBackground,
-                              size: 50,
-                              text: (index + 1).toString(),
-                            ),
-                          ),
-                        );
-                      }),
-                    ),
-                    const SizedBox(height: 20),
-                    AppLargeText(
-                      text: 'Description',
-                      color: Colors.black.withOpacity(0.8),
-                      size: 20,
-                    ),
-                    const SizedBox(height: 5),
-                    AppText(
-                      text:
-                          'Hidden beach or Playa del amor, is a protected area in Islas Marietas. Legend tells him that in military practice a cannon hit the island, a cave fell and this beautiful beach was created. ',
-                      color: Colors.grey,
-                    )
-                  ],
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              bottom: 30,
-              left: 20,
-              right: 20,
-              child: Row(
-                children: [
-                  AppButtons(
-                    color: Colors.black,
-                    backgroundColor: Colors.white,
-                    boderColor: Colors.grey,
-                    size: 60,
-                    isIcon: true,
-                    icon: Icons.favorite_border,
+                Positioned(
+                  bottom: 30,
+                  left: 20,
+                  right: 20,
+                  child: Row(
+                    children: [
+                      AppButtons(
+                        color: Colors.black,
+                        backgroundColor: Colors.white,
+                        boderColor: Colors.grey,
+                        size: 60,
+                        isIcon: true,
+                        icon: Icons.favorite_border,
+                      ),
+                      const SizedBox(width: 20),
+                      ResponsiveBtn(
+                        isResponsive: true,
+                      ),
+                    ],
                   ),
-                  const SizedBox(width: 20),
-                  ResponsiveBtn(
-                    isResponsive: true,
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
